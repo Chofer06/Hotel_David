e86c5fdb22a9f33ad224bd371de5fd880df1cac1
David Mueses <davidmueses0@gmail.com>
2020-12-18 12:28:38 -0500

    Hotel_Final

--- resources/views/facturacion/factura.blade.php
+++ resources/views/facturacion/factura.blade.php
@@ -3,6 +3,6 @@
     <br><h1 align="center"><strong>FACTURACION</strong></h1>
     <div align="center">
 		<img src="{{url('imagenes/16.jpg')}}" height="50" width="300" >
-	</div>
+	</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,5 +19,20 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script type="text/javascript" src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    
+        <br><br>
+        <footer class="page-footer font-small black darken-3" >
+            <div class="card text-white bg-primary mb-3" style="max-width: 90rem;">
+                <div class="card-header" align="center">© 2020 Copyright: davidmueses0@gmail.com</div>
+                <div class="card-body">
+                    <h5 class="card-title" align="center"><strong>HOTEL LA RIVIERA</strong></h5>
+                    <p class="card-text" align="center">Las Mejores Vacaciones</p>
+                </div>
+                <div class="card-header" align="right" style="background: black">Diseñado Por: David Mueses<br>Yolima Quiroz<br>Universidad De Nariño</div>
+            </div>
+        </footer>
+
     </body>
+
+    
 </html>
